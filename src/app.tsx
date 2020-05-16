2287381858cf1486c5a41bebc7d81d902a615092
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-16 16:49:48 -0700

    Setup dev environment

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app.scss';
+
+const App: React.StatelessComponent<{}> = () => (
+    <div className="container">
+        <h1>Hello World, React!</h1>
+    </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: 'src/index.tsx',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    resolve: {
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                    'prettier-loader',
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            cache: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            }
+        ]
+    }
+}
