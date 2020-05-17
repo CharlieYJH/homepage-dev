53ac3d5086049997d37db933fdf98610f91fc133
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-16 21:08:58 -0700

    Restructure files

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './app.scss';
 
-const App: React.StatelessComponent<{}> = () => (
+const App: React.FC<{}> = () => (
     <div className="container">
         <h1>Hello World, React!</h1>
     </div>
