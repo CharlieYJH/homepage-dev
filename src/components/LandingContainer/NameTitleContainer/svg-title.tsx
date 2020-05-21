03144436a8314d11beda05505b824d9ea063bc3a
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-21 01:54:36 -0700

    Adjust subtitle position

--- src/components/LandingContainer/NameTitleContainer/svg-title.tsx
+++ src/components/LandingContainer/NameTitleContainer/svg-title.tsx
@@ -7,7 +7,7 @@ interface Title {
 
 export const SvgTitle: React.FC<Title> = ({ title }) => (
     <svg className="svg-title" viewBox="0 0 199.5 12.5">
-        <text x="0" y="0" dx="5" dy="6" letterSpacing="0.7">
+        <text x="0" y="0" dx="35" dy="8" letterSpacing="0.7">
             {title}
         </text>
     </svg>
