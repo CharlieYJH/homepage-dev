0bb982c29c5a89d401ffae161d2e9f45f63e90cd
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-21 01:02:39 -0700

    Adjust image and text sizes

--- src/components/LandingContainer/NameTitleContainer/svg-title.tsx
+++ src/components/LandingContainer/NameTitleContainer/svg-title.tsx
@@ -7,7 +7,7 @@ interface Title {
 
 export const SvgTitle: React.FC<Title> = ({ title }) => (
     <svg className="svg-title" viewBox="0 0 199.5 12.5">
-        <text x="10" y="7.5" dominantBaseline="middle">
+        <text x="10" y="0" dy="6">
             {title}
         </text>
     </svg>
