563adfd7b04a28ca84e439431c774845c28115d0
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-23 02:31:10 -0700

    Use an empty element to pad subtitle div

--- src/components/LandingPage/components/Subtitle/subtitle.tsx
+++ src/components/LandingPage/components/Subtitle/subtitle.tsx
@@ -9,7 +9,9 @@ interface Title {
 export const Subtitle: React.FC<Title> = ({ titles }) => (
     <div className={styles.container}>
         <div className={styles.title}>
-            <div className={styles.spacer} />
+            <div className={styles.spacer}>
+                <SvgTitle title={''} />
+            </div>
             <div className={styles.carousel}>
                 {titles.map((title) => (
                     <SvgTitle key={title} title={title} />
