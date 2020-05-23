3d5896f9fdac82ffd34d2ff5b45795210bdaf4c0
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-23 02:28:01 -0700

    Changed subtitle to css modules

--- src/components/LandingPage/components/Subtitle/subtitle.tsx
+++ src/components/LandingPage/components/Subtitle/subtitle.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { SvgTitle } from './svg-title';
-import './subtitle.scss';
+import styles from './subtitle.module.scss';
 
 interface Title {
     titles: string[];
 }
 
 export const Subtitle: React.FC<Title> = ({ titles }) => (
-    <div className="landing-title-container">
-        <div className="landing-title">
-            <div className="landing-title-stretcher" />
-            <div className="landing-title-carousel">
+    <div className={styles.container}>
+        <div className={styles.title}>
+            <div className={styles.spacer} />
+            <div className={styles.carousel}>
                 {titles.map((title) => (
                     <SvgTitle key={title} title={title} />
                 ))}
