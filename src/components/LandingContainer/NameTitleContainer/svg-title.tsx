701a26617b89638f0a128e90899c85a98d0ccbe3
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-22 21:45:08 -0700

    Changed svg title to css modules

--- src/components/LandingContainer/NameTitleContainer/svg-title.tsx
+++ src/components/LandingContainer/NameTitleContainer/svg-title.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './svg-title.scss';
+import styles from './svg-title.module.scss';
 
 interface Title {
     title: string;
 }
 
 export const SvgTitle: React.FC<Title> = ({ title }) => (
-    <svg className="svg-title" viewBox="0 0 199.5 12.5">
+    <svg className={styles.title} viewBox="0 0 199.5 12.5">
         <text x="0" y="0" dx="35" dy="8" letterSpacing="0.7">
             {title}
         </text>
