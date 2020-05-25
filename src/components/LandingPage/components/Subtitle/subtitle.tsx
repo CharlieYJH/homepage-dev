818b9e694d35b60c2daaba554b8c5cdead154059
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-25 00:49:04 -0700

    UI adjustments

--- src/components/LandingPage/components/Subtitle/subtitle.tsx
+++ src/components/LandingPage/components/Subtitle/subtitle.tsx
@@ -15,6 +15,11 @@ export const Subtitle: React.FC<Title> = ({ titles }) => {
       <div className={context.lightTheme ? styles.spacerLight : styles.spacerDark}>
         <SvgTitle title={''} />
       </div>
+      {Array(3).fill(
+        <div className={context.lightTheme ? styles.spacer : styles.spacer}>
+          <SvgTitle title={''} />
+        </div>
+      )}
       <Carousel
         className={styles.carousel}
         scrollTimeout={3000}
