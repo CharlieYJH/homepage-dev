9b2b3c5a05789103150ffb091ad2821d6bd3a526
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-24 23:36:13 -0700

    Merge title and nav colours

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navigation } from './components/Navigation';
 import { ThemeIcon } from './components/ThemeIcon';
+import { ThemeContext } from '../../providers/ThemeProvider';
 import styles from './header.module.scss';
 
 const navlinks = ['about me', 'experience', 'contact me'];
 
 export const Header: React.FC<{}> = () => (
-  <div className={styles.container}>
+  <div className={useContext(ThemeContext).lightTheme ? styles.light : styles.dark}>
     <div className={styles.nav}>
       <Navigation links={navlinks} />
     </div>
