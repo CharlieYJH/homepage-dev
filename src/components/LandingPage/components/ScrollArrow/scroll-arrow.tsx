4b82d8afd5fa7b6a7e7f1f29702465265297906f
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-24 22:20:37 -0700

    Add dark theme to scroll arrow

--- src/components/LandingPage/components/ScrollArrow/scroll-arrow.tsx
+++ src/components/LandingPage/components/ScrollArrow/scroll-arrow.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../../../providers/ThemeProvider';
 import styles from './scroll-arrow.module.scss';
 
-export const ScrollArrow: React.FC<{}> = () => (
-  <div className={styles.container}>
-    <div className={styles.bar} />
-    <div className={styles.arrow} />
-  </div>
-);
+export const ScrollArrow: React.FC<{}> = () => {
+  const context = useContext(ThemeContext);
+  return (
+    <div className={styles.container}>
+      <div className={context.lightTheme ? styles.barLight : styles.barDark} />
+      <div className={context.lightTheme ? styles.arrowLight : styles.arrowDark} />
+    </div>
+  );
+};
