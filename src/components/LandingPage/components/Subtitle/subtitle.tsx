fbe421ea7d14ebc52b146bd8c68d31a2e21811cb
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-24 22:13:15 -0700

    Add dark theme to subtitle

--- src/components/LandingPage/components/Subtitle/subtitle.tsx
+++ src/components/LandingPage/components/Subtitle/subtitle.tsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SvgTitle } from './svg-title';
 import { Carousel } from '../../../Carousel';
+import { ThemeContext } from '../../../../providers/ThemeProvider';
 import styles from './subtitle.module.scss';
 
 interface Title {
   titles: string[];
 }
 
-export const Subtitle: React.FC<Title> = ({ titles }) => (
-  <div className={styles.container}>
-    <div className={styles.spacer}>
-      <SvgTitle title={''} />
+export const Subtitle: React.FC<Title> = ({ titles }) => {
+  const context = useContext(ThemeContext);
+  return (
+    <div className={styles.container}>
+      <div className={context.lightTheme ? styles.spacerLight : styles.spacerDark}>
+        <SvgTitle title={''} />
+      </div>
+      <Carousel
+        className={styles.carousel}
+        scrollTimeout={3000}
+        transitionDuration={300}
+        classActive={
+          context.lightTheme
+            ? styles.carouselItemActiveLight
+            : styles.carouselItemActiveDark
+        }
+        classInactive={
+          context.lightTheme
+            ? styles.carouselItemInactiveLight
+            : styles.carouselItemInactiveDark
+        }
+        overflow={2}
+      >
+        {titles.map((title) => (
+          <SvgTitle key={title} title={title} className={styles.carouselItem} />
+        ))}
+      </Carousel>
     </div>
-    <Carousel
-      className={styles.carousel}
-      scrollTimeout={3000}
-      transitionDuration={300}
-      classActive={styles.carouselItemActive}
-      classInactive={styles.carouselItemInactive}
-      overflow={2}
-    >
-      {titles.map((title) => (
-        <SvgTitle key={title} title={title} className={styles.carouselItem} />
-      ))}
-    </Carousel>
-  </div>
-);
+  );
+};
