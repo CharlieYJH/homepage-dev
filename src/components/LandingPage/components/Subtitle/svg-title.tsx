a1730170cb1d800ffac065c624ead729cc107bbb
CharlieYJH <charlieyinjunhao@hotmail.com>
2020-05-26 02:17:47 -0700

    Adjust subtitle size and spacing

--- src/components/LandingPage/components/Subtitle/svg-title.tsx
+++ src/components/LandingPage/components/Subtitle/svg-title.tsx
@@ -9,7 +9,7 @@ interface Properties {
 
 export const SvgTitle: React.FC<Properties> = (props) => (
   <svg className={classnames(styles.title, props.className)} viewBox="0 0 199.5 12.5">
-    <text x="0" y="0" dx="35" dy="8" letterSpacing="0.7">
+    <text x="0" y="0" dx="49" dy="8" letterSpacing="0.7">
       {props.title}
     </text>
   </svg>
