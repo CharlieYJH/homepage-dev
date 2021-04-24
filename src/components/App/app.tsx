1b292f2936dca6b540e9baf0ce9e0dcbb5048db7
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-04-24 16:45:22 -0700

    Add progress bar

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Header } from '../Header';
 import { LandingPage } from '../LandingPage';
 import { ContentContainer } from '../ContentContainer';
+import { MenuBar } from '../MenuBar';
 import { AboutMe } from '../AboutMe';
 import { useScrollPosition } from '../../hooks/ScrollPosition';
 import styles from './app.module.scss';
@@ -15,8 +16,7 @@ export const App: React.FC<{}> = () => {
       console.log(pos);
     },
     [pos],
-    document.getElementById('root'),
-    100
+    document.getElementById('root')
   );
 
   return (
@@ -27,6 +27,13 @@ export const App: React.FC<{}> = () => {
           <LandingPage />
         </div>
         <ContentContainer>
+          <div className={styles.menuContainer}>
+            <MenuBar
+              progress={
+                Math.abs(pos.y) / (document.body.clientHeight - window.innerHeight)
+              }
+            />
+          </div>
           <div className={styles.aboutContainer}>
             <AboutMe />
           </div>
---
+++ src/components/MenuBar/menu-bar.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './menu-bar.module.scss';
+
+const items = [
+  {
+    name: 'about me',
+    href: '#about-me',
+  },
+  {
+    name: 'experience',
+    href: '#experience',
+  },
+  {
+    name: 'projects',
+    href: '#projects',
+  },
+  {
+    name: 'contact me',
+    href: '#contact-me',
+  },
+];
+
+export const MenuBar: React.FC<{ progress: number }> = (props) => {
+  const position = Math.floor(props.progress / (1 / (items.length - 1)));
+  return (
+    <div className={styles.menu}>
+      <div className={classnames(styles.itemContainer, styles.dotsContainer)}>
+        {items.map((item, i) => (
+          <div key={item.name} className={styles.item}>
+            <div
+              className={classnames(
+                styles.dot,
+                position == i ? styles.dotActive : '',
+                position > i ? styles.dotDone : ''
+              )}
+            />
+          </div>
+        ))}
+        <div
+          className={styles.progress}
+          style={{ width: ((100 / items.length) * (items.length - 1)).toString() + '%' }}
+        >
+          <div
+            className={styles.progressFill}
+            style={{ width: Math.min(100, props.progress * 100).toString() + '%' }}
+          />
+        </div>
+      </div>
+      <div className={styles.itemContainer}>
+        {items.map((item, i) => (
+          <div key={item.name} className={styles.item}>
+            <div
+              className={classnames(styles.text, position === i ? styles.textActive : '')}
+            >
+              {item.name}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
