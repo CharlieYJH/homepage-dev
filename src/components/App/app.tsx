d77d683d0fe657423afbb126c29217dc6973899c
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-04-24 23:16:30 -0700

    Implement simple progression using checkpoints

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -10,8 +10,31 @@ import styles from './app.module.scss';
 
 export const App: React.FC<{}> = () => {
   const [pos, setPos] = useState({ x: 0, y: 0 });
+
   const menuRef = useRef(null);
   const [showMenu, setShowMenu] = useState(false);
+
+  const checkpoints = [useRef(null), useRef(null), useRef(null), useRef(null)];
+  const [progress, setProgress] = useState(0);
+
+  const updateProgress = (): void => {
+    for (let i = 0; i < checkpoints.length; i++) {
+      if (!checkpoints[i].current) {
+        return;
+      }
+    }
+
+    const start = checkpoints[0].current.getBoundingClientRect().top;
+    const end = checkpoints[checkpoints.length - 1].current.getBoundingClientRect().top;
+    const len = end - start;
+
+    if (start > 0) {
+      setProgress(0);
+      return;
+    }
+
+    setProgress(Math.abs(checkpoints[0].current.getBoundingClientRect().top) / len);
+  };
 
   useScrollPosition(
     (currpos: { x: number; y: number }) => {
@@ -24,15 +47,14 @@ export const App: React.FC<{}> = () => {
 
   useEffect(() => {
     setShowMenu(menuRef.current.getBoundingClientRect().top <= 0);
-  }, [menuRef, pos]);
+    updateProgress();
+  }, [menuRef, pos, ...checkpoints]);
 
   return (
     <div>
       <Header />
       <div className={classnames(styles.menu, showMenu ? styles.menuActive : '')}>
-        <MenuBar
-          progress={Math.abs(pos.y) / (document.body.clientHeight - window.innerHeight)}
-        />
+        <MenuBar progress={progress} />
       </div>
       <div className={styles.container}>
         <div className={styles.landingContainer}>
@@ -40,9 +62,12 @@ export const App: React.FC<{}> = () => {
         </div>
         <div ref={menuRef} className={styles.separator} />
         <ContentContainer>
-          <div className={styles.aboutContainer}>
+          <div ref={checkpoints[0]} className={styles.aboutContainer}>
             <AboutMe />
           </div>
+          <div ref={checkpoints[1]} className={styles.placeholderLong} />
+          <div ref={checkpoints[2]} className={styles.placeholderShort} />
+          <div ref={checkpoints[3]} className={styles.placeholderShort} />
         </ContentContainer>
       </div>
     </div>
