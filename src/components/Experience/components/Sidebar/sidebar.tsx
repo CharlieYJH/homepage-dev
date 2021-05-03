09ac6d11997b671aed699913ad48a62727c58a35
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-05-02 22:28:03 -0700

    Move from and to tags outside of sidebar

--- src/components/Experience/components/Sidebar/sidebar.tsx
+++ src/components/Experience/components/Sidebar/sidebar.tsx
@@ -1,60 +1,38 @@
-import React, { useState, useEffect, useRef } from 'react';
-import classnames from 'classnames';
-import { useScrollPosition } from '../../../../hooks/ScrollPosition';
+import React from 'react';
 import { TagScroller } from '../TagScroller';
 import styles from './sidebar.module.scss';
 
-export const Sidebar: React.FC<{}> = () => {
-  const toTags = ['present', '2020'];
-  const fromTags = ['2020', '2018'];
+interface Properties {
+  toTags: string[];
+  fromTags: string[];
+  activeFrom: string;
+  activeTo: string;
+}
 
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-  const [active, setActive] = useState(0);
-
-  const setActiveTag = (pos: number): void => {
-    if (pos > 2500) {
-      setActive(1);
-    } else {
-      setActive(0);
-    }
-  };
-
-  useScrollPosition(
-    (currpos: { x: number; y: number }) => setPos(currpos),
-    [pos],
-    document.getElementById('root'),
-    50
-  );
-
-  useEffect(() => {
-    setActiveTag(Math.abs(pos.y));
-  }, [pos]);
-
-  return (
-    <div className={styles.sidebar}>
-      <div className={styles.textHolder}>
-        <div className={styles.title}>experience</div>
-        <div className={styles.contentContainer}>
-          <TagScroller
-            entries={fromTags}
-            active={active}
-            carouselClass={styles.carousel}
-            entryActiveClass={styles.contentActive}
-            entryInactiveClass={styles.contentInactive}
-          />
-        </div>
-        <div className={styles.subtitle}>to</div>
-        <div className={styles.contentContainer}>
-          <TagScroller
-            entries={toTags}
-            active={active}
-            carouselClass={styles.carousel}
-            entryActiveClass={styles.contentActive}
-            entryInactiveClass={styles.contentInactive}
-          />
-        </div>
+export const Sidebar: React.FC<Properties> = (props) => (
+  <div className={styles.sidebar}>
+    <div className={styles.textHolder}>
+      <div className={styles.title}>experience</div>
+      <div className={styles.contentContainer}>
+        <TagScroller
+          entries={props.fromTags}
+          active={props.fromTags.indexOf(props.activeFrom)}
+          carouselClass={styles.carousel}
+          entryActiveClass={styles.contentActive}
+          entryInactiveClass={styles.contentInactive}
+        />
       </div>
-      <div className={styles.divider} />
+      <div className={styles.subtitle}>to</div>
+      <div className={styles.contentContainer}>
+        <TagScroller
+          entries={props.toTags}
+          active={props.toTags.indexOf(props.activeTo)}
+          carouselClass={styles.carousel}
+          entryActiveClass={styles.contentActive}
+          entryInactiveClass={styles.contentInactive}
+        />
+      </div>
     </div>
-  );
-};
+    <div className={styles.divider} />
+  </div>
+);
--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Sidebar } from './components/Sidebar';
 import { ExperienceItem } from './components/ExperienceItem';
+import { useScrollPosition } from '../../hooks/ScrollPosition';
 import styles from './experience.module.scss';
 
 const entries = [
@@ -13,6 +14,8 @@ const entries = [
       'Participated in cross-team discussions to evaluate project feasibilities and identify potential issues',
       'Balanced multiple projects while ensuring high code quality and on-time completion of requirements',
     ],
+    from: '2021',
+    to: 'present',
   },
   {
     title: 'Fortinet',
@@ -23,6 +26,8 @@ const entries = [
       'Improved the stability of FortiClient Linux significantly by fixing critical issues raised by various users',
       'Stayed up-to-date with and learned new technologies that can better improve product performance',
     ],
+    from: '2018',
+    to: '2021',
   },
   {
     title: 'UBC Sailbot',
@@ -33,14 +38,51 @@ const entries = [
       'Developed and debugged embedded firmware for the STM32F4 microcontroller using Linux tools',
       'Implemented reliable data transfer in a real-time system for up to 127 separate data nodes using C',
     ],
+    from: '2016',
+    to: '2017',
   },
 ];
 
 export const Experience: React.FC<{}> = () => {
+  const fromTags: string[] = [];
+  const toTags: string[] = [];
+
+  entries.forEach((entry) => {
+    fromTags.push(entry.from);
+    toTags.push(entry.to);
+  });
+
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [active, setActive] = useState({ from: fromTags[0], to: toTags[0] });
+
+  const setActiveTags = (pos: number): void => {
+    let idx = 0;
+
+    if (pos > 2500) {
+      idx = 1;
+    }
+
+    setActive({ from: fromTags[idx], to: toTags[idx] });
+  };
+
+  useScrollPosition(
+    (currpos: { x: number; y: number }) => setPos(currpos),
+    [pos],
+    document.getElementById('root'),
+    50
+  );
+
+  useEffect(() => setActiveTags(Math.abs(pos.y)), [pos]);
+
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
-        <Sidebar />
+        <Sidebar
+          fromTags={fromTags}
+          toTags={toTags}
+          activeFrom={active.from}
+          activeTo={active.to}
+        />
       </div>
       <div className={styles.contentContainer}>
         <div className={styles.content}>
