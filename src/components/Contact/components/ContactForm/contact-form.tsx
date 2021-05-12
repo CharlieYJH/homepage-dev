37e5da931547060b70f40b8aa3f47451fa780d17
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-05-11 21:55:01 -0700

    Add axios request on submit

--- src/components/Contact/components/ContactForm/contact-form.tsx
+++ src/components/Contact/components/ContactForm/contact-form.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { SingleLineInput } from '../SingleLineInput';
 import { MessageBox } from '../MessageBox';
 import styles from './contact-form.module.scss';
 
 export const ContactForm: React.FC<{}> = () => {
+  const [submitState, setSubmitState] = useState({
+    submitted: false,
+    submitting: false,
+    ok: true,
+  });
+
+  const handleResponse = (ok: boolean, form: HTMLFormElement): void => {
+    setSubmitState({
+      submitted: true,
+      submitting: false,
+      ok: ok,
+    });
+
+    if (ok) {
+      form.reset();
+    }
+  };
+
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement> & { target: HTMLFormElement }
+  ): void => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    axios({
+      method: 'post',
+      url: 'http://localhost:8090/post',
+      data: new FormData(form),
+    })
+      .then((_) => {
+        handleResponse(true, form);
+      })
+      .catch((_) => {
+        handleResponse(false, form);
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className={styles.inputContainer}>
         <SingleLineInput
           label="name"
