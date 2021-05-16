3a84509cfd75d5c71e72d3f0c1b5db141b8a79f6
CharlieYJH <charlieyinjunhao@hotmail.com>
2021-05-16 15:52:00 -0700

    Render after window load event

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import { WindowSizeContextProvider } from './providers/WindowSizeProvider';
 
-ReactDOM.render(
-  <WindowSizeContextProvider>
-    <App />
-  </WindowSizeContextProvider>,
-  document.getElementById('root')
-);
+window.addEventListener('load', (_) => {
+  ReactDOM.render(
+    <WindowSizeContextProvider>
+      <App />
+    </WindowSizeContextProvider>,
+    document.getElementById('root')
+  );
+});
